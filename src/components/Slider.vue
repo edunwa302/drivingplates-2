31ec7d95c490be2b26b2b74a8c6d4e20dd434c77
edunwa <edunwa302@gmail.com>
2022-09-14 00:15:46 +0100

    added slick slider

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -10,15 +10,27 @@
         <Navigation />
       </template>
     </Carousel> -->
+    <VueSlickCarousel :arrows="true" :dots="true">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </VueSlickCarousel>
   </section>
 </template>
 <script>
 // import { Carousel, Slide, Navigation } from "vue3-carousel";
 // import "vue3-carousel/dist/carousel.css";
+import Navbar from "./Navbar.vue";
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+  // optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
 export default {
-  name: "Autoplay",
+  name: "slider",
   components: {
+    VueSlickCarousel,
     // Carousel,
     // Slide,
     // Navigation,
