c7404f7e3514706f65f85c58d29700d318cad2d0
edunwa <edunwa302@gmail.com>
2022-09-13 23:28:13 +0100

    setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <p class="bg-red-500 text-gray-600">hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+};
+</script>
+
+<style>
+</style>
