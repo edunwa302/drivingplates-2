98cbe6db7e3ba58c7cd653861413814a94033540
edunwa <edunwa302@gmail.com>
2022-09-14 00:09:09 +0100

    migrated all components to vue2

---
+++ src/components/Headline.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="pt-12 pb-28">
+    <h2 class="max-w-[35rem] mx-auto text-center text-[28px] sm:text-4xl leading-[3rem] tracking-[.07777778px] sm:tracking-[.1rem] sm:leading-[4.5rem]">
+      drivingplates.com llc is the worlds leading source 
+    </h2>
+    <h2 class="max-w-[35rem] mx-auto text-center text-[28px] sm:text-4xl leading-[3rem] tracking-[.07777778px] sm:tracking-[.1rem] sm:leading-[4.5rem]">
+      for high quality 360<sup>o</sup> motion environments 
+    </h2>
+    <h2 class="max-w-[35rem] mx-auto text-center text-[28px] sm:text-4xl leading-[3rem] tracking-[.07777778px] sm:tracking-[.1rem] sm:leading-[4.5rem]">
+      and stock driving footage from around the globe. 
+    </h2>
+  </section>
+</template> 
+
+<style scoped>
+    sup{
+        font-family: sans-serif;
+        font-size: 45%;
+        top: -1rem;
+    }
+</style>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,77 @@
+<template>
+   <section id="bg-container" class="jumbotron">
+    <div class="fixed video-container">
+      <!-- <video autoplay muted loop class="video">
+        <source src="/video/diving.mp4" type="video/mp4" />
+      </video> -->
+    </div>
+    <navbar />
+    <!-- <div class="h-[25vh] sm:h-[40vh] md:h-[65vh] xl:h-[55vh] 2xl:h-[50vh]"></div> -->
+    <div style="padding-bottom: 33.6%"></div>
+    <div
+      class="flex justify-end px-4 sm:px-12 md:px-16 lg:px-20 xl:px-[100px] 2xl:px-0 py-4 2xl:max-w-[1700px] 2xl:mx-auto"
+    >
+      <a
+        href="#"
+        class="text-white border-2 text-[10px] uppercase font-bold tracking-[.3rem] px-[25px] py-[12px] block border-white hover:bg-white transition duration-3000 ease-linear hover:text-[#191606] group-hover:"
+      >
+        find footage
+      </a>
+    </div>
+  </section>
+</template>
+<script>
+import Navbar from "./Navbar.vue";
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+<style scoped>
+.jumbotron {
+  font-family: "Heebo", Arial, Helvetica, sans-serif;
+}
+.video-container {
+  background-color: #7e3a3a;
+  min-width: 100%;
+  width: auto;
+  min-height: 80vh;
+  z-index: -1;  
+}
+.video {
+  max-width: 800px;
+  background: url("/video/video-palceholder.png");
+  background-position: center;
+  background-size: cover;
+  background-attachment: fixed;
+  transition: all .3s ease;
+}
+
+@media screen and (min-width: 640px) {
+  .video {
+    max-width: 1200px;
+  }
+}
+@media screen and (min-width: 768px) {
+  .video {
+    max-width: 1400px;
+  }
+}
+@media screen and (min-width: 1100px) {
+  .video {
+    max-width: 2000px;
+  }
+}
+@media screen and (min-width: 1100px) {
+  .video {
+    max-width: 110vw;
+  }
+}
+@media screen and (min-width: 1900px) {
+  .video {
+    max-width: none;
+    width: 100vw;
+  }
+}
+</style>
